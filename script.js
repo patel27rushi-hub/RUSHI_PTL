e74d115933d191b7c39e8369ac470ca391e2b428
patel27rushi-hub <patel27rushi@gmail.com>
2025-12-17 12:30:06 +0530

    R

---
+++ script.js
@@ -0,0 +1,18 @@
+// Smooth scrolling
+document.querySelectorAll('a[hreff^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute
+        ('href')).scrollIntoview({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Navbar scroll effect
+window,addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    window.scrollY > 50 ?
+    navbar.style.backgroundColor = 'rgba(10,10,10, 0.98)' :
+    navbar.style.backgroundColor = 'rgba(10,10,10, 0.95)';
+})
